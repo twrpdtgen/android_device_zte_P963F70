b0ab10df6192ff6211b0032fff8c89aaafc4dcd1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-27 14:47:40 +0200

    P963F70: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: zte Model: ZTE Blade A31RU Script version: 2.4.0

---
+++ omni_P963F70.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from P963F70 device
+$(call inherit-product, device/zte/P963F70/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := P963F70
+PRODUCT_NAME := omni_P963F70
+PRODUCT_BRAND := ZTE
+PRODUCT_MODEL := ZTE Blade A31RU
+PRODUCT_MANUFACTURER := zte
